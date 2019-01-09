af6f5b735cc9788666131dc7eed95c2b009ea901
nt <nataliia.tolmachova@gmail.com>
2019-01-09 21:36:23 +0100

    Initial commit

---
+++ src/main/java/com/nt/controllers/GetterInjectedController.java
@@ -0,0 +1,21 @@
+package com.nt.controllers;
+
+import com.nt.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class GetterInjectedController {
+    private GreetingService greetingService;
+
+    public String sayHello(){
+        return greetingService.sayGreeting();
+    }
+
+    @Autowired
+    public void setGreetingService(@Qualifier("getterGreetingService") GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+}
---
+++ src/main/java/com/nt/controllers/PropertyInjectedController.java
@@ -0,0 +1,20 @@
+package com.nt.controllers;
+
+import com.nt.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class PropertyInjectedController {
+
+    @Autowired
+    @Qualifier("greetingServiceImpl")
+    public GreetingService greetingServiceImpl;
+
+    public String sayHello(){
+        return greetingServiceImpl.sayGreeting();
+    }
+
+}
